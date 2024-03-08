04b019e0ab65379d45264c0236c2f38ed25db736
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2024-03-08 20:28:21 +0700

    solution of students doing homework at a given time

---
+++ algorithm/busyStudent.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function (startTime, endTime, queryTime) {
+  let count = 0;
+
+  for (let i = 0; i < startTime.length; i++) {
+    if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+console.log(busyStudent([1, 2, 3], [3, 2, 7], 4));
+console.log(busyStudent([4], [4], 5));
